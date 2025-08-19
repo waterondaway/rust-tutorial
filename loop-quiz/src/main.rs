9c988999712283b95c9c9779eb71bf0712f34859
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-08-19 10:25:34 +0700

    rename from rust-tutorial to fundamental

--- loop-quiz/src/main.rs
+++ loop-quiz/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
     // Question #1 | Sum 1 - 10 with Loop .
-    let mut sum: i32;
+    let mut count_loop: i32 = 0;
+    let mut sum: i32 = 0;
     loop {
-        println!("Hello World!");
+        count_loop = count_loop + 1;
+        sum = sum + count_loop;
+        if count_loop == 10 {
+            break;
+        } else {
+            continue;
+        }
     }
     println!("Question #1");
     println!("Answer of Question #4 'Sum 1 - 10' : {}\n\n", sum);
