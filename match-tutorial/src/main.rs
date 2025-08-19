e5f4b8ecfeae275d60cd39671507d9f897b28cde
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-08-19 14:08:54 +0700

    match second quiz test

--- match-tutorial/src/main.rs
+++ match-tutorial/src/main.rs
@@ -9,5 +9,20 @@ fn main() {
         9 | 10 | 11 => println!("Autumn"),
         _ => println!("Invalid Month")
     }
+
+    // Question #2 | Finding Type of Transport .
+    enum Transport {
+        Car(String, i32),
+        Bike(i32),
+        Airplane { model: String, capacity: i32 },
+        Walking,
+    }
+    let my_transport = Transport::Car(String::from("Tesla"), 20);
+    match my_transport {
+        Transport::Car(name, speed) => println!("Car {} with top speed {} km/h", name, speed),
+        Transport::Bike(speed) => println!("Bike with top speed {} km/h", speed),
+        Transport::Airplane {model, capacity} => println!("Airplane {} with capacity {} passengers", model, capacity),
+        Transport::Walking => println!("Just walking...")
+    }
     
 }
