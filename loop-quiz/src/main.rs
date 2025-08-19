e41d3edc10f98cf7fc7e4a9659e276e5adb9d9fb
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-08-19 10:41:46 +0700

    loop quiz test

--- loop-quiz/src/main.rs
+++ loop-quiz/src/main.rs
@@ -7,10 +7,35 @@ fn main() {
         sum = sum + count_loop;
         if count_loop == 10 {
             break;
-        } else {
-            continue;
         }
     }
     println!("Question #1");
-    println!("Answer of Question #4 'Sum 1 - 10' : {}\n\n", sum);
+    println!("Answer of Question #1 'Sum 1 - 10' : {}\n\n", sum);
+
+
+    // Question #2 | Factorial .
+    let mut factorial_number: i32 = 5;
+    let mut answer: i32 = 1;
+    loop {
+        answer = answer * factorial_number;
+        factorial_number = factorial_number - 1;
+        if factorial_number == 0 {
+            break;
+        }
+    }
+    println!("Question #2");
+    println!("Answer of Question #2 'Factorial Number' : {}\n\n", answer);
+
+    // Question #3 | Count Down Boom! .
+    println!("Question #3");
+    let mut loop_count:i32 = 5;
+    loop {
+        println!("{}", loop_count);
+        loop_count = loop_count - 1;
+        if loop_count == 0 {
+            println!("Boom!");
+            break;
+        }
+    }
+
 }
