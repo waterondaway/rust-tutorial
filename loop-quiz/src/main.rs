bcd7d6f45f925f52c6789428f911c5c4b0ba57bd
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-08-19 10:20:41 +0700

    change folder name from cargo-project to rust-tutorial

---
+++ loop-quiz/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Question #1 | Sum 1 - 10 with Loop .
+    let mut sum: i32;
+    loop {
+        println!("Hello World!");
+    }
+    println!("Question #1");
+    println!("Answer of Question #4 'Sum 1 - 10' : {}\n\n", sum);
+}
